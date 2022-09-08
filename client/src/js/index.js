803b5c5698d5d33450044ca84e09455efddda2c9
unknown <philbranch243@gmail.com>
2022-09-08 15:39:14 -0500

    can add through form and display db data to cards

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,7 @@
 // import js files
-import './form.js';
-import './submit';
+import { toggleForm, clearForm } from './form.js';
+
+import './cards';
 // import initDb function, et. al.
 import { initDb, getDb, postDb } from './database';
 // import images
@@ -12,13 +13,52 @@ window.addEventListener('load', function () {
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
   initDb();
+  fetchCards();
   //place these functions here for testing, but will move later
   // getDb();
   // postDb("Phil", "phil@email.com", 3145555555, "Bear");
   // getDb();
 });
+
+  // Form functionality
+  const form = document.getElementById("formToggle");
+  const newContactButton = document.getElementById("new-contact");
+  let submitBtnToUpdate = false;
+  let profileId;
+  
+  newContactButton.addEventListener('click', event => {
+    toggleForm()
+   })
+  
+  form.addEventListener('submit', event => {
+    // Handle data
+    event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+  
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+  
+    fetchCards();
+      // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+  
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
+  });
+
 // import css
 import '../css/index.css';
 // import bootstrap
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { fetchCards } from './cards';
