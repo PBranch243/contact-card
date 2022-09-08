723bae197ba22f9ca871d6e5e15016f9d41dd99b
unknown <philbranch243@gmail.com>
2022-09-08 15:01:16 -0500

    get & post methods working with indexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,17 +1,21 @@
 // import js files
 import './form.js';
 import './submit';
-// import initDb function
-import { initDb } from './database';
+// import initDb function, et. al.
+import { initDb, getDb, postDb } from './database';
 // import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 window.addEventListener('load', function () {
-  initDb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
+  initDb();
+  //place these functions here for testing, but will move later
+  // getDb();
+  // postDb("Phil", "phil@email.com", 3145555555, "Bear");
+  // getDb();
 });
 // import css
 import '../css/index.css';
